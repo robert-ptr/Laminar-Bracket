4b6235956d21212b068c10c19fbe0227dcb615b1
Robert Petre <petrerobertcristian@gmail.com>
2026-02-02 23:06:48 +0200

    Clean up analysis.py by removing unused code
    Removed commented-out code related to StarCraft dataset loading and printing available files.

--- analysis.py
+++ analysis.py
@@ -18,7 +18,7 @@ print(df.head())
 
 path = kagglehub.dataset_download("sammahoney/esa-anomaly-dataset")
 
-print(f"Dataset descărcat la: {path}")
+print(f"{path}")
 
 mission = 1
 channel_id = 1
@@ -28,18 +28,6 @@ with open(channel_file, 'rb') as f:
     data = pickle.load(f)
 
 df = pd.DataFrame(data)
-
-print(df.shape)
-print(df.head())
-
-path = kagglehub.dataset_download("sfu-summit/starcraft-ii-replay-analysis")
-
-#print("Path to dataset files:", f"{path}/starcraft.csv")
-files = os.listdir(path)
-print("Fișiere disponibile în folder:")
-for f in files:
-    print(f"- {f}")
-df = pd.read_csv(f"{path}/starcraft.csv")
 
 print(df.shape)
 print(df.head())
